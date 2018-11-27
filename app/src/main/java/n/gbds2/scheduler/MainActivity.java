08f86065a0680edb4553fb652840b17f7e803930
quadroue4 <gbds234@kookmin.ac.kr>
2018-11-27 20:28:36 +0900

    sub액티비티의 새로운 알람정보를 메인액티비티로 가져오는것까지 완료. 이를 내부프리퍼런스로 저장해 메인xml에 띄우게 해야함.

--- app/src/main/java/n/gbds2/scheduler/MainActivity.java
+++ app/src/main/java/n/gbds2/scheduler/MainActivity.java
@@ -1,29 +1,47 @@
 package n.gbds2.scheduler;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button addButton = (Button)findViewById(R.id.Add);
-        addButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
+        Button addButton = (Button) findViewById(R.id.Add);
+        name = (TextView) findViewById(R.id.textView8);
+        name.setText("fuc2k");
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SubActivity.class);
-                startActivityForResult(intent,3000);
+                startActivityForResult(intent, 3000);
 
             }
         });
+
     }
 
 
-    public void add_button(){
 
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        name.setText("no");
+        if (requestCode == 3000) {
+            if(resultCode == Activity.RESULT_OK){
+                String name=data.getStringExtra("name");
+                String time=data.getStringExtra("time");
+                String start=data.getStringExtra("start");
+                String end=data.getStringExtra("end");
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                name.setText("no");
+            }
+        }
     }
 }
--- app/src/main/java/n/gbds2/scheduler/SubActivity.java
+++ app/src/main/java/n/gbds2/scheduler/SubActivity.java
@@ -1,10 +1,15 @@
 package n.gbds2.scheduler;
 
 import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SubActivity extends AppCompatActivity {
 
@@ -13,10 +18,23 @@ public class SubActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
         Button finishButton = (Button)findViewById(R.id.Finish);
+        final TextView name = (TextView) findViewById(R.id.Name);
+        final TextView time = (TextView) findViewById(R.id.Time);
+        final TextView start = (TextView) findViewById(R.id.DueStart);
+        final TextView end = (TextView) findViewById(R.id.DueEnd);
+
         finishButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
-                Intent intent = new Intent(SubActivity.this,MainActivity.class);
-                startActivityForResult(intent,3000);
+                //Intent intent = new Intent(SubActivity.this,MainActivity.class);
+                //startActivityForResult(intent,3000);
+                Intent intent = new Intent();
+                intent.putExtra("name",name.getText().toString());
+                intent.putExtra("time",time.getText().toString());
+                intent.putExtra("start",start.getText().toString());
+                intent.putExtra("end",end.getText().toString());
+
+                setResult(RESULT_OK,intent);
+                finish();
 
             }
         });
